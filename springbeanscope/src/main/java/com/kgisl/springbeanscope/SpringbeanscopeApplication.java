4a732b1f8252876c15dd3623f3a8b6c5a6067c7c
Raji23 <rajalakshmi.lingam96@gmail.com>
2018-03-08 17:14:40 +0530

    Add files via upload

---
+++ springbeanscope/src/main/java/com/kgisl/springbeanscope/SpringbeanscopeApplication.java
@@ -0,0 +1,12 @@
+package com.kgisl.springbeanscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeanscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbeanscopeApplication.class, args);
+	}
+}
